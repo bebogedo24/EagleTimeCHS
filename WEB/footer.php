d86ac79e95b887cc349335542bc683c0a65b391a
David Aylaian <DavidAylaian@users.noreply.github.com>
2016-12-19 18:14:33 -0500

    Rebranded watermark \

--- WEB/footer.php
+++ WEB/footer.php
@@ -1,6 +1,6 @@
 
 </body>
-<small id="watermark">Created by Zach Sents</small>
+<small id="watermark">Created by the <a href="http://appliedvisuals.x10host.com/">Applied Visuals Team</a></small>
 </html>
 <?php
 ob_end_flush();
