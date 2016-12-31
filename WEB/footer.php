907e06646fedb8a744da695df2f19c05b806dad2
David Aylaian <DavidAylaian@users.noreply.github.com>
2016-12-30 20:58:39 -0500

    Minor changes to watermark

--- WEB/footer.php
+++ WEB/footer.php
@@ -1,6 +1,6 @@
 
 </body>
-<small id="watermark">Created by the <a href="http://appliedvisuals.x10host.com/">Applied Visuals Team</a></small>
+<small id="watermark">Created by the <a href="http://appliedvisuals.x10host.com/" style="color: #DBDBDB; text-decoration: underline">Applied Visuals Team</a></small>
 </html>
 <?php
 ob_end_flush();
